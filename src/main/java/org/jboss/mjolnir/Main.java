37392522168ad1787cad886430c34d99212e446c
Navin Surtani <nsurtani@redhat.com>
2013-03-22 10:57:06 +0800

    Messing with Main class

--- src/main/java/org/jboss/mjolnir/Main.java
+++ src/main/java/org/jboss/mjolnir/Main.java
@@ -4,7 +4,12 @@ import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.client.GitHubRequest;
 import org.eclipse.egit.github.core.client.GitHubResponse;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Main class to be used for this project. In time, we might not need a Main class.
@@ -15,8 +20,16 @@ import java.io.IOException;
 public class Main {
 
     private static final String BASE_URL = "api.github.com";
+    private static final String PROPS_FILE = "users.properties";
 
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        try {
+            properties.load(streamFromFile());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
         GitHubRequest request = new GitHubRequest();
         request.setUri(BASE_URL);
 
@@ -32,6 +45,13 @@ public class Main {
             throw new RuntimeException("There has been an error in executing the request to Github. " +
                     "Your URL might be wrong. " + BASE_URL);
         }
+    }
+
+    private static InputStream streamFromFile() {
+        String propsFile = Main.class.getResource(PROPS_FILE).getFile();
+        System.out.println("Properties file: " + propsFile);
+
+        return null;
     }
 
 }
