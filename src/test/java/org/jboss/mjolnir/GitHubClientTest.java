57b287cbb6275a134a7c248dbad88bb34aed2c53
Tomas Hofman <thofman@redhat.com>
2015-01-15 15:25:03 +0100

    followup for #17 - added test class with sample GitHubClient usage

---
+++ src/test/java/org/jboss/mjolnir/GitHubClientTest.java
@@ -0,0 +1,58 @@
+package org.jboss.mjolnir;
+
+
+import org.eclipse.egit.github.core.Team;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.jboss.mjolnir.githubclient.ExtendedTeamService;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Sample test class illustrating the use of GitHubClient.
+ *
+ * @author Tomas Hofman (thofman@redhat.com)
+ */
+@Ignore // only intended for development / illustrational purposes, not to be run during the build process
+public class GitHubClientTest {
+
+    private static String TOKEN = "???"; // provide GitHub "personal access token" (needs to have admin:org persmission)
+    private static int TEAM_ID = 1223195;
+    private static String ORG_NAME = "thofmantestorg";
+    private static String USERNAME = "testtest22";
+
+    private GitHubClient gitHubClient = new GitHubClient();
+    private ExtendedTeamService teamService = new ExtendedTeamService(gitHubClient);
+
+    @Before
+    public void setUp() {
+        gitHubClient.setOAuth2Token(TOKEN);
+    }
+
+    @Test
+    public void listTeams() throws IOException {
+        final List<Team> teams = teamService.getTeams(ORG_NAME);
+        System.out.println(teams);
+    }
+
+    @Test
+    public void addTeamMember() throws IOException {
+        teamService.addMember(TEAM_ID, USERNAME);
+    }
+
+    @Test
+    public void addTeamMembersip() throws IOException {
+        teamService.addMembership(TEAM_ID, USERNAME);
+    }
+
+    @Test
+    public void getMembers() throws IOException {
+        List<User> members = teamService.getMembers(TEAM_ID);
+        System.out.println(members);
+    }
+
+}
