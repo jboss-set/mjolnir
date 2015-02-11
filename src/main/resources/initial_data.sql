a454b654997e0edefb34214100438f99e8e4549e
Tomas Hofman <thofman@redhat.com>
2015-02-11 10:23:29 +0100

    anonymized initial data

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,7 +1,8 @@
-insert into application_parameters (param_name, param_value) values ('github.token', '148f1d234b8323edc72daac9a355df0cf59e8bea');
+insert into application_parameters (param_name, param_value) values ('github.token', '***');
 insert into application_parameters (param_name, param_value) values ('ldap.url', 'ldap://ldap.nrt.redhat.com');
+insert into application_parameters (param_name, param_value) values ('infinispan.path', '/tmp/infinispan.store');
 
-insert into github_orgs (name) values ('thofmantestorg');
+insert into github_orgs (name) values ('some org');
 
-insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'PushTeam', 1223195);
-insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'Owners', 1223193);
+insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'some team', 123);
+insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'some other team', 1234);
