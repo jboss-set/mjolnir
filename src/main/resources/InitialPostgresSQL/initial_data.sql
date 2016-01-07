5eb11099d04dfe63945f9b529cd21cd30403ffa3
xstefank <mjolnir122@gmail.com>
2016-01-07 13:54:35 +0100

    postgre scripts update

--- src/main/resources/InitialPostgresSQL/initial_data.sql
+++ src/main/resources/InitialPostgresSQL/initial_data.sql
@@ -7,5 +7,5 @@ insert into application_parameters (param_name, param_value) values ('krb5.kdc',
 
 insert into github_orgs (name) values ('some org');
 
-insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'some team', 123);
-insert into github_teams (org_id, name, github_id) values (sq_github_orgs.currval, 'some other team', 1234);
+insert into github_teams (org_id, name, github_id) values (currval('sq_github_orgs'), 'some team', 123);
+insert into github_teams (org_id, name, github_id) values (currval('sq_github_orgs'), 'some other team', 1234);
