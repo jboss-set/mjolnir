e7d6189ae1ef6f448a7acdf52d8ef35f5aeafe35
Tomas Hofman <thofman@redhat.com>
2021-09-15 11:16:31 +0200

    Remove debugging messages in postconfigure.sh

--- s2i-config/extensions/postconfigure.sh
+++ s2i-config/extensions/postconfigure.sh
@@ -3,20 +3,6 @@
 #set -x
 ENV_DIR=/etc/eap-environment
 
-# Copy OID adapter configuration file
-#echo Copying $ENV_DIR/secure-deployments to $JBOSS_HOME/standalone/configuration/secure-deployments
-#cp $ENV_DIR/secure-deployments $JBOSS_HOME/standalone/configuration/secure-deployments
-
-# Trigger Keycload adapter configuration
-#echo Configuring Keycloack
-#source $JBOSS_HOME/bin/launch/openshift-common.sh
-#export CONFIG_FILE=$JBOSS_HOME/standalone/configuration/standalone-openshift.xml
-#source $JBOSS_HOME/bin/launch/keycloak.sh
-#configure_cli_keycloak
-
-echo "Grepping for postgres":
-grep postgres $JBOSS_HOME/standalone/configuration/standalone-openshift.xml
-
 # Run EAP modifications CLI script
 echo Configuring EAP
 $JBOSS_HOME/bin/jboss-cli.sh --file=$ENV_DIR/eap-config.cli
