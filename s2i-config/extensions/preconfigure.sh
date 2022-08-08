da36ab50ba195210c966f740195e79b149aec635
Tomas Hofman <thofman@redhat.com>
2022-08-08 18:43:19 +0200

    Keycloak adapter configured via CLI, not secure-deployment file
    Workaround for https://issues.redhat.com/browse/CLOUD-4121

--- s2i-config/extensions/preconfigure.sh
+++ s2i-config/extensions/preconfigure.sh
@@ -2,5 +2,5 @@
 
 ENV_DIR=/etc/eap-environment
 
-echo Copying $ENV_DIR/secure-deployments to $JBOSS_HOME/standalone/configuration/secure-deployments
-cp $ENV_DIR/secure-deployments $JBOSS_HOME/standalone/configuration/secure-deployments
+#echo Copying $ENV_DIR/secure-deployments to $JBOSS_HOME/standalone/configuration/secure-deployments
+#cp $ENV_DIR/secure-deployments $JBOSS_HOME/standalone/configuration/secure-deployments
